074f42ad83f8fae9429f6842face742fdadecb7b
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-08-11 10:43:07 +0200

    Add webpack& start project

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,9 @@
+import styles from './TextInput.module.scss';
+
+const TextInput = () => {
+    return (
+      <input className={styles.input} placeholder="Search..." type="text" />
+    );
+};
+
+export default TextInput;
