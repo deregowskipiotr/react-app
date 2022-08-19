54cf04ad8438073a9208835083263fa431d91483
Piotr Deregowski <deregowskipiotr@interia.pl>
2022-08-19 17:01:25 +0200

    Add redux/ modified components List&&Column

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
@@ -6,5 +9,13 @@ import 'font-awesome/css/font-awesome.min.css';
 
 
 
-const root = createRoot(document.querySelector('#root')); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
+
+
